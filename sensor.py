adfc78bc28e60f005d7b377b83a576161de4c0b7
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-19 15:41:20 +0100

    Prepare sensor json config

--- sensor.py
+++ sensor.py
@@ -13,14 +13,14 @@ override_conf_from_env(conf, 'log_level')
 override_conf_from_env(conf, 'pin')
 override_conf_from_env(conf, 'wait_time')
 
-WAIT_TIME = cast_int(conf['wait_time'])
-DATA_PIN = cast_int(conf['pin'])
+_wait_time = cast_int(conf['wait_time'])
+_data_pin = cast_int(conf['pin'])
 
 while True:
-    humid, temp = dht22.read_humidity_and_temperature(DATA_PIN)
+    humid, temp = dht22.read_humidity_and_temperature(_data_pin)
     if is_not_empty(humid) and is_not_empty(temp):
         log_msg("INFO", "Temperature={0:0.1f}*C Humidity={1:0.1f}%".format(temp, humid))
     else:
         log_msg("ERROR", "Failed to retrieve values...")
 
-    sleep(WAIT_TIME)
+    sleep(_wait_time)
