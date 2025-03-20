be31d8519a2a7fc88313ee3abba456f606454927
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-20 16:04:17 +0100

    Switch to adafruit

--- sensor.py
+++ sensor.py
@@ -1,10 +1,11 @@
-from time import sleep
-from pigpio_dht import DHT22
+import adafruit_dht
+import board
 
-from utils.common import is_empty_key, cast_int
+from time import sleep
+
+from utils.common import cast_int
 from utils.config import get_config, override_conf_from_env
 from utils.logger import log_msg
-from utils.pigpiod import start_pigpiod
 
 conf = get_config("sensor")
 
@@ -14,17 +15,12 @@ override_conf_from_env(conf, 'wait_time')
 
 _wait_time = cast_int(conf['wait_time'])
 _data_pin = cast_int(conf['pin'])
-_retries = cast_int(conf['retries'])
-_timeout = cast_int(conf['timeout'])
 
-start_pigpiod()
-sensor = DHT22(_data_pin, timeout_secs=_timeout)
+sensor = adafruit_dht.DHT22(_data_pin)
 
 while True:
-    data = sensor.read(retries=_retries)
-    if is_empty_key(data, 'temp_c') or is_empty_key(data, 'humidity'):
-        log_msg("ERROR", f"Error reading values: data={data}")
-        continue
+    temp = sensor.temperature
+    humidity = sensor.humidity
 
-    log_msg("INFO", f"Temperature={data['temp_c']} Humidity={data['humidity']}")
+    log_msg("INFO", f"Temperature={temp} Humidity={humidity}")
     sleep(_wait_time)
