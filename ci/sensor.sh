c27d6af3c1111fc576e25f4d3c4c33abb47f9970
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-21 13:03:22 +0100

    Add CI/CD deployment on the rpi

---
+++ ci/sensor.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+venv_path="./vdemo"
+
+ps -ef|awk '($0 ~ "sensor.py" && $0 ~ "python" && $0 !~ "awk"){system ("kill -9 "$2)}'
+[[ -d $venv_path ]] || python3 -m venv ./vdemo
+
+./vdemo/bin/pip install --upgrade pip setuptools wheel
+./vdemo/bin/pip install -r requirements.txt
+./vdemo/bin/python sensor.py & disown
