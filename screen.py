14eef32ce9aa6c0fdfdeb9b03b4603d936828f45
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-22 12:10:12 +0100

    Fix: display issue

--- screen.py
+++ screen.py
@@ -1,4 +1,12 @@
+import os
 import tkinter as tk
+
+from utils.common import is_empty
+from utils.logger import log_msg
+
+if is_empty(os.environ.get('DISPLAY')):
+    log_msg("DEBUG", "[screen] no display found, using: 0.0")
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 window = tk.Tk()
 window.title("Hello")
