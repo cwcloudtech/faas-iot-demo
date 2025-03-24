e8e5593ebebaf761a60f6d68d2f717e23ea6e692
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-24 13:48:54 +0100

    Fix: display environment variable

--- utils/gui.py
+++ utils/gui.py
@@ -6,6 +6,10 @@ from utils.common import is_empty
 from utils.logger import log_msg
 
 def display(title, message):
+    if is_empty(os.environ.get('DISPLAY')):
+        log_msg("DEBUG", "[screen] no display found, using: 0.0")
+        os.environ.__setitem__('DISPLAY', ':0.0')
+
     window = tk.Tk()
     window.title(title)
     window.attributes("-fullscreen", True)
