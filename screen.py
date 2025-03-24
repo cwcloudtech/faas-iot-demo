aedd8199e09910841e21b8713ccf8940c8b6c83b
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-24 10:54:04 +0100

    Refactoring using gui utils

--- screen.py
+++ screen.py
@@ -1,18 +1,3 @@
-import os
-import tkinter as tk
+from utils.gui import display
 
-from utils.common import is_empty
-from utils.logger import log_msg
-
-if is_empty(os.environ.get('DISPLAY')):
-    log_msg("DEBUG", "[screen] no display found, using: 0.0")
-    os.environ.__setitem__('DISPLAY', ':0.0')
-
-window = tk.Tk()
-window.title("Hello")
-window.attributes("-fullscreen", True)
-window.bind("<Escape>", lambda _: window.attributes("-fullscreen", False))
-
-label = tk.Label(window, text="Hello, Raspberry Pi!")
-label.pack(padx=20, pady=20)
-window.mainloop()
+display("Hello", "🧪")
---
+++ utils/gui.py
@@ -0,0 +1,20 @@
+import os
+
+import tkinter as tk
+
+from utils.common import is_empty
+from utils.logger import log_msg
+
+def display(title, message):
+    if is_empty(os.environ.get('DISPLAY')):
+        log_msg("DEBUG", "[screen] no display found, using: 0.0")
+        os.environ.__setitem__('DISPLAY', ':0.0')
+
+    window = tk.Tk()
+    window.title(title)
+    window.attributes("-fullscreen", True)
+    window.bind("<Escape>", lambda _: window.attributes("-fullscreen", False))
+
+    label = tk.Label(window, text=message)
+    label.pack(padx=20, pady=20)
+    window.mainloop()
