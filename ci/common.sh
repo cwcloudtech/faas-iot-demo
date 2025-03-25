ce73ebd8ccb7a044047e351145b04c8952023810
Idriss Neumann <idriss.neumann@comwork.io>
2025-03-25 15:45:19 +0100

    Fix: environment variables CICD

--- ci/common.sh
+++ ci/common.sh
@@ -23,15 +23,12 @@ create_venv() {
         for var in ${vars[@]}; do
             sed -i 
             val="$(printenv $var)"
-            for conf in ${conf[@]}; do
+            for conf in ${confs[@]}; do
                 sed -i "s/${var}_changeit/${val}/g" "${conf}.json"
             done
             echo "export ${var}=\"${val}\"" >> $VENV_PATH/bin/activate
         done
-        
-        echo "export CWCLOUD_DEMO_callback_url=\"${CWCLOUD_DEMO_callback_url}\"" >> $VENV_PATH/bin/activate
-        echo "export =\"${CWCLOUD_DEMO_callback_username}\"" >> $VENV_PATH/bin/activate
-        echo "export ='${CWCLOUD_DEMO_callback_password}'" >> $VENV_PATH/bin/activate
+
         source $VENV_PATH/bin/activate
         $VENV_PATH/bin/pip install --upgrade pip setuptools wheel
     fi
